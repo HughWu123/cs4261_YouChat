bf753930d0d79eed2bd785823c849daf6dda45f5
Micking <wang.wang@gatech.edu>
2019-09-03 20:46:02 -0400

    Replace reposory

--- app/src/main/java/com/example/hughiscool/youchat/MainActivity.java
+++ app/src/main/java/com/example/hughiscool/youchat/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.hughiscool.youchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
     }
 }
